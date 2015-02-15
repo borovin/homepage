717209d64b1cf4656d7d244518c527780eaecffc
Andrew Borovin <a@borovin.com>
2015-02-15 14:07:42 +0300

    add test

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-var app = require('./index.js');
+var express = require('express');
+var app = express();
 
-app.listen(process.env.port || 5000);
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+});
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 window.CONFIG = {
-    clientVersion: '1423997868693'
+    clientVersion: '1423998342644'
 };
