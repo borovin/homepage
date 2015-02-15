4ea45d8ad4a99e812fb80182355bd7a732283797
Andrew Borovin <a@borovin.com>
2015-02-15 14:29:33 +0300

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./index.js');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 
 app.listen(app.get('port') || 8080, function(){
     console.log("Node app is running at: " + app.get('port'))
