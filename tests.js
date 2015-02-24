2846585dad885f6aec9efd5ca2005351406701f7
Andrew Borovin <a@borovin.com>
2015-02-24 03:46:04 +0300

    add test class

--- tests.js
+++ tests.js
@@ -4,8 +4,8 @@ casper.test.begin('Tests', function suite(test) {
     casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36');
     casper.viewport(1280, 1024);
 
-    casper.waitForSelector('[test="name"]', function() {
-        test.assertSelectorHasText('[test="name"]', 'Андрей Боровин');
+    casper.waitForSelector('.test-name', function() {
+        test.assertSelectorHasText('.test-name', 'Андрей Боровин');
     });
 
     casper.run(function() {
