0ecb7197f9c884e30b46ab2db336d4ccfca08b81
Andrew Borovin <a@borovin.com>
2015-02-24 03:10:07 +0300

    update casper tests

--- tests.js
+++ tests.js
@@ -1,11 +1,11 @@
 casper.test.begin('Tests', function suite(test) {
 
-    casper.start('http://test.borovin.com/');
+    casper.start(casper.cli.options.host);
     casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36');
     casper.viewport(1280, 1024);
 
-    casper.then(function() {
-        test.assertTitle('Andrey Borovin', 'title is correct');
+    casper.waitForSelector('.page__headerTitle', function() {
+        test.assertSelectorHasText('.page__headerTitle', 'Андрей Боровин');
     });
 
     casper.run(function() {
