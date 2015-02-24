6e9d435e5a801fec38a7b4d1e04d344621bff407
Andrew Borovin <a@borovin.com>
2015-02-24 04:30:10 +0300

    update casper timeout

--- tests/phone.js
+++ tests/phone.js
@@ -1,5 +1,7 @@
 casper.test.begin('Phone', function suite(test) {
 
+    casper.options.waitTimeout = 10000;
+
     casper.start(casper.cli.options.host);
     casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36');
     casper.viewport(1280, 1024);
