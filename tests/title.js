826e8ac4ea1b944eba70af9a137870c1cb6d12d3
Andrew Borovin <a@borovin.com>
2015-02-24 04:43:02 +0300

    update tests

--- tests/title.js
+++ tests/title.js
@@ -1,18 +1,15 @@
-casper.test.begin('Email', function suite(test) {
-
-    casper.options.waitTimeout = 10000;
+casper.test.begin('Title', function suite(test) {
 
     casper.start(casper.cli.options.host);
     casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36');
     casper.viewport(1280, 1024);
 
     casper.then(function() {
-        this.waitForSelector('[test="email"]', function(){
-            test.assertSelectorHasText('[test="email"]', 'a@borovin.com');
-        });
+        test.assertTitle('Andrey Borovin', 'title is correct');
     });
 
     casper.run(function() {
         test.done();
-    }, 1000);
+    });
+
 });
