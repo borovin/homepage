215b02699125ee7695a3dde8fd25d99f3fdc37e6
Andrew Borovin <a@borovin.com>
2015-03-01 14:46:05 +0300

    fix scroll in ff

--- src/kit/page/page.js
+++ src/kit/page/page.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     //requirements
     var Block = require('kit/block/block');
 
@@ -6,31 +6,31 @@ define(function(require, exports, module) {
         el: '#page',
         scrollTop: 0,
         globalEvents: {
-            'scroll': function(){
+            'scroll': function () {
 
-                var scrollTop = document.body.scrollTop,
-                    scrollHeight = document.body.scrollHeight,
-                    windowHeight = window.innerHeight,
-                    scrollMax = scrollHeight - windowHeight,
-                    fixedHeight = this.fixedContent.clientHeight,
+                var scrollTop = $(document).scrollTop(),
+                    documentHeight = $(document).height(),
+                    windowHeight = $(window).height(),
+                    scrollMax = documentHeight - windowHeight,
+                    fixedHeight = this.fixedContent.offsetHeight,
                     scrollOffset = windowHeight - fixedHeight;
 
-                if (scrollTop < 0){
+                if (scrollTop < 0) {
                     scrollTop = 0;
                 }
 
-                if (scrollTop > scrollMax){
+                if (scrollTop > scrollMax) {
                     scrollTop = scrollMax;
                 }
 
                 var scrollDistance = this.scrollTop - scrollTop,
                     fixedTopPosition = this.fixedContent.offsetTop + scrollDistance;
 
-                if (fixedTopPosition >= 0){
+                if (fixedTopPosition >= 0) {
                     fixedTopPosition = 0;
                 }
 
-                if (fixedTopPosition <= scrollOffset){
+                if (fixedTopPosition <= scrollOffset) {
                     fixedTopPosition = scrollOffset;
                 }
 
@@ -40,7 +40,7 @@ define(function(require, exports, module) {
 
             }
         },
-        initialize: function(){
+        initialize: function () {
 
             var page = this;
 
@@ -48,52 +48,52 @@ define(function(require, exports, module) {
 
             page.loading();
 
-            return $.when(Block.prototype.initialize.apply(page, arguments)).then(function(){
+            return $.when(Block.prototype.initialize.apply(page, arguments)).then(function () {
                 return page.fetch();
             });
         },
-        fetch: function(resorces) {
+        fetch: function (resorces) {
             var page = this,
                 resorces = resorces || {};
 
-            var collectionsList = _(page.collections).filter(function(collection, name) {
+            var collectionsList = _(page.collections).filter(function (collection, name) {
                 return !resorces.collections || resorces.collections.indexOf(name) != -1;
             }).value();
 
-            var modelsList = _(page.models).filter(function(model, name) {
+            var modelsList = _(page.models).filter(function (model, name) {
                 return (!resorces.models || resorces.models.indexOf(name) != -1) && model && model.id;
             }).value();
 
             var dataList = collectionsList.concat(modelsList);
 
-            page.fetchList = _.map(dataList, function(data) {
+            page.fetchList = _.map(dataList, function (data) {
                 return (data && typeof data.fetch === 'function') ? data.fetch() : data;
             });
 
-            return $.when.apply($, page.fetchList).then(function() {
+            return $.when.apply($, page.fetchList).then(function () {
                 delete page.fetchList;
             });
         },
-        render: function(){
+        render: function () {
             var render = Block.prototype.render.apply(this, arguments),
                 page__content = this.el.querySelectorAll('.page__content');
 
             this.leftContent = page__content[0];
             this.rightContent = page__content[1];
 
-            if ($(this.leftContent).height() > $(this.rightContent).height()){
-                this.leftContent.classList.add('page__content_fixed');
-                this.fixedContent = this.leftContent;
-            } else {
+            if (this.leftContent.offsetHeight > this.rightContent.offsetHeight) {
                 this.rightContent.classList.add('page__content_fixed');
                 this.fixedContent = this.rightContent;
+            } else {
+                this.leftContent.classList.add('page__content_fixed');
+                this.fixedContent = this.leftContent;
             }
 
             return render;
         },
-        loading: function(start) {
+        loading: function (start) {
 
-            if (start || start === undefined){
+            if (start || start === undefined) {
                 this.el.classList.add('loading');
             } else {
                 this.el.classList.remove('loading');
