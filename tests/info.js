0d7a6a5bcf0b4f3eae79dc1bab7d14c4006b4254
Andrew Borovin <a@borovin.com>
2015-03-03 21:24:47 +0300

    fix photo

--- tests/info.js
+++ tests/info.js
@@ -15,6 +15,7 @@ casper.test.begin('Info block', function (test) {
     });
 
     casper.waitForSelector('#page[data-status="loaded"]', function () {
+
         test.assertSelectorHasText('[locator="phone"]', '+79218902868');
         test.assertSelectorHasText('[locator="email"]', 'a@borovin.com');
         test.assertSelectorHasText('[locator="skype"]', 'andrew.borovin');
