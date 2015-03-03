a9f6f44d6de3a1b7244d3bff1685e95976f2c273
Andrew Borovin <a@borovin.com>
2015-03-03 23:33:49 +0300

    update resume tests

---
+++ tests/resume.js
@@ -0,0 +1,42 @@
+casper.test.begin('Resume', function (test) {
+
+    casper.then(function () {
+        casper.evaluate(function () {
+
+            window.history.replaceState({}, document.title, '/');
+
+            requirejs({
+                shim: {
+                    main: ['resources/info/mocks/get']
+                }
+            }, ['main']);
+
+        });
+    });
+
+    casper
+        .waitForSelector('#page[data-status="loaded"]')
+        .then(function () {
+            test.assertSelectorHasText('[locator="name"]', 'Андрей Боровин');
+        })
+        .then(function () {
+            test.assertSelectorHasText('[locator="phone"]', '+79218902868');
+        })
+        .then(function () {
+            test.assertSelectorHasText('[locator="email"]', 'a@borovin.com');
+        })
+        .then(function () {
+            test.assertSelectorHasText('[locator="skype"]', 'andrew.borovin');
+        })
+        .then(function () {
+            test.assertEquals(casper.getElementAttribute('[locator="githubLink"]', 'href'), 'https://github.com/borovin', 'github link href is ok');
+        })
+        .then(function () {
+            phantomcss.screenshot('.resume', 'resume');
+        });
+
+    casper.run(function () {
+        test.done();
+    });
+
+});
